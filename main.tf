093f4aafe99b1e416846c3d158696fb94fc58d17
rajuparipelly <154118030+rajuparipelly@users.noreply.github.com>
2023-12-18 12:08:51 +0530

    new s3 bucket

--- main.tf
+++ main.tf
@@ -1,20 +1,9 @@
-data "aws_ami" "api_ami" {
-  most_recent = true
-  filter {
-    name = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
+provider "aws"{
+  profile = "default"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "aws" {
-  ami = "data.aws_ami.app_ami.id"
-  instance_type = "t3.micro"
-
-  tags = {
-    name = "helloworld"
-  }
+resource "aws_s3_bucket" "tf-learn" {
+  bucket = "test-1231934812"
+  acl = "private"
 }
