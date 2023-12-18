932a8324041dfdb1493c40ee4830679f1c8fe2ab
rajuparipelly <154118030+rajuparipelly@users.noreply.github.com>
2023-12-18 11:32:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "api_ami" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resources "aws_instance" "aws" {
+  ami = "data.aws_ami.app_ami.id"
+  instance_type = "t3.micro"
+
+  tags = {
+    name = "helloworld"
+  }
+}
